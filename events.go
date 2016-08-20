fcf59c93a28b324a9b00af5fab938ef4764ee43a
R. Díaz de León <leon@elinter.net>
2016-08-19 22:47:22 -0500

    Updated DB queries for events

--- events.go
+++ events.go
@@ -19,34 +19,8 @@ type Event struct {
 
 type Events []Event
 
-// List the first 25 users found in the database
-func listEvents() (events Events, err error) {
-	// TODO: Add pagination
-	rows, err := db.Conn.Query("SELECT eid, name, start, duration, created, published" +
-		" FROM events ORDER BY published desc LIMIT 25",
-	)
-
-	if err != nil {
-		return
-	}
-
-	defer rows.Close()
-
-	events = make([]Event, 0)
-
-	for rows.Next() {
-		var event Event
-		err = rows.Scan(&(event.EID), &(event.Name), &(event.Start), &(event.Duration), &(event.Created), &(event.Published))
-		if err != nil {
-			break
-		}
-		events = append(events, event)
-	}
-
-	return
-}
-
-func loadEvent(eid int) (event Event, err error) {
+// Returns the event eid from the database
+func getEvent(eid int) (event Event, err error) {
 	err = db.Conn.QueryRow(
 		"SELECT name, start, duration, created, published FROM events WHERE eid = $1",
 		eid).Scan(&(event.Name), &(event.Start), &(event.Duration), &(event.Created), &(event.Published))
@@ -60,7 +34,68 @@ func loadEvent(eid int) (event Event, err error) {
 
 }
 
-func saveEvent(event Event) {
+// Creates and updates an event in the database
+func (event Event) Save() (err error) {
+	if event.EID > 0 {
+		err = db.QueryRow(
+			"INSERT INTO events(name, start, duration, created, published) VALUES($1, $2, $3, $4, $5) RETURNING eid;",
+			event.Name,
+			event.Start,
+			event.Duration,
+			event.Created,
+			event.Published,
+		).Scan(&event.EID)
+
+		if err != nil {
+			event.EID = -1
+		}
+	} else {
+		err = db.Query(
+			"UPDATE events SET name, start, duration, created, published) VALUES($1, $2, $3, $4, $5);",
+			event.Name,
+			event.Start,
+			event.Duration,
+			event.Created,
+			event.Published,
+		)
+	}
+
+	return
+}
+
+// List the first 25 users found in the database
+func listEvents() (events Events, err error) {
+	// TODO: Add pagination
+	rows, err := db.Conn.Query(
+		`SELECT eid, name, start, duration, created, published
+		 FROM events ORDER BY published desc LIMIT 25`)
+
+	if err != nil {
+		return
+	}
+
+	defer rows.Close()
+
+	events = make([]Event, 0)
+
+	for rows.Next() {
+		var event Event
+		err = rows.Scan(
+			&(event.EID),
+			&(event.Name),
+			&(event.Start),
+			&(event.Duration),
+			&(event.Created),
+			&(event.Published),
+		)
+
+		if err != nil {
+			break
+		}
+		events = append(events, event)
+	}
+
+	return
 }
 
 func ListEvents(w http.ResponseWriter, r *http.Request) {
