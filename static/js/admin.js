546a36dc997a27e1cf38eeda51c915b846dfa086
R. Díaz de León <leon@elinter.net>
2016-08-22 22:40:27 -0500

    Added basic admin files

---
+++ static/js/admin.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function init() {
+    var container = document.getElementById('container');
+    var menu = document.getElementById('menu');
+    var eventForm = document.getElementById('event-form');
+
+}
+
+document.addEventListener('DOMContentLoaded', init);
