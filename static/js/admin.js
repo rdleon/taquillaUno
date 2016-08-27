ba95b57ff48cfd54957a4dfdfb508ce744abba3b
R. Díaz de León <leon@elinter.net>
2016-08-27 17:39:19 -0500

    Added login form

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,10 +1,52 @@
 'use strict'
 
-function init() {
-    var container = document.getElementById('container');
-    var menu = document.getElementById('menu');
-    var eventForm = document.getElementById('event-form');
+function run() {
+    var container,
+        menu,
+        loggedIn = false,
+        forms = {};
 
+    menu = document.getElementById('menu');
+    container = document.getElementById('container');
+
+    function initLoginForm(form) {
+    }
+
+    function initUserForm(form) {
+    }
+
+    function initEventForm(form) {
+    }
+
+    function init() {
+        forms.login = document.getElementById('loginForm');
+        forms.user = document.getElementById('userForm');
+        forms.event = document.getElementById('eventForm');
+
+        initLoginForm(forms.login);
+        initUserForm(forms.user);
+        initEventForm(forms.event);
+
+        if (!loggedIn) {
+            showForm('login');
+        } else {
+            showForm('event');
+        }
+    }
+
+    function showForm(formName) {
+        // Toggle hidden in the selected form
+        // hide all others.
+        for (name in forms) {
+           if (name == formName) {
+               removeClass(forms[name], "hidden");
+           } else {
+               addClass(forms[name], "hidden")
+           }
+       }
+    }
+
+    init();
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', run);
--- static/js/libutil.js
+++ static/js/libutil.js
@@ -1,5 +1,23 @@
 "use strict";
 
+var hasClass, addClass, removeClass;
+
+if ('classList' in document.documentElement) {
+    hasClass = function (el, className) { return el.classList.contains(className); };
+    addClass = function (el, className) { el.classList.add(className); };
+    removeClass = function (el, className) { el.classList.remove(className); };
+} else {
+    hasClass = function (el, className) {
+        return new RegExp('\\b'+ className+'\\b').test(el.className);
+    };
+    addClass = function (el, className) {
+        if (!hasClass(el, className)) { el.className += ' ' + className; }
+    };
+    removeClass = function (el, className) {
+        el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+    };
+}
+
 function isFunction(func) {
     var type = {};
     return func && type.toString.call(func) === '[object Function]';
