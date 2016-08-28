d7206a1d381f2135990e2e441625ff493dc53b65
R. Díaz de León <leon@elinter.net>
2016-08-27 22:44:01 -0500

    Added documentation to JS functions

--- static/js/admin.js
+++ static/js/admin.js
@@ -11,6 +11,11 @@ function run() {
     container = document.getElementById('container');
     http = new HTTP();
 
+    /**
+     * Defines the basic functionality for
+     * the login form
+     * @param {element} form - The DOM representation of the form
+     */
     function initLoginForm(form) {
         function onLogin(res) {
             if (res.token) {
@@ -21,6 +26,7 @@ function run() {
         }
 
         function onLoginFail(status, res) {
+            // TODO(rdleon): show failure message
             console.log(status, res);
         }
 
@@ -38,18 +44,48 @@ function run() {
         });
     }
 
+    /**
+     * Defines the basic functionality for the user form
+     * for adding and modifying users.
+     * @param {element} form - The DOM representation of the form
+     */
     function initUserForm(form) {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
         });
     }
 
+    /**
+     * Defines the basic functionality for the user form
+     * for adding and modifying users.
+     * @param {element} form - The DOM representation of the form
+     */
     function initEventForm(form) {
         form.addEventListener('submit', function (event) {
             event.preventDefault();
         });
     }
 
+    /**
+     * Used to select which form to show at any given time in the UI
+     * @param {string} formName - The name of the form to show
+     */
+    function showForm(formName) {
+        // Toggle hidden in the selected form
+        // hide all others.
+        for (name in forms) {
+           if (name == formName) {
+               removeClass(forms[name], "hidden");
+           } else {
+               addClass(forms[name], "hidden")
+           }
+       }
+    }
+
+    /**
+     * This is the function that bootstraps the forms and the
+     * bulk of the web UI.
+     */
     function init() {
         forms.login = document.getElementById('loginForm');
         forms.user = document.getElementById('userForm');
@@ -64,18 +100,6 @@ function run() {
         } else {
             showForm('event');
         }
-    }
-
-    function showForm(formName) {
-        // Toggle hidden in the selected form
-        // hide all others.
-        for (name in forms) {
-           if (name == formName) {
-               removeClass(forms[name], "hidden");
-           } else {
-               addClass(forms[name], "hidden")
-           }
-       }
     }
 
     init();
